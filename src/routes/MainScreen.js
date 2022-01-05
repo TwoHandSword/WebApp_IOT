8ae4cd1bc5c28cde70d5cc8fac29821ba7fabab3
downy <TeamBehindDowny@gmail.com>
2022-01-06 01:21:45 +0900

    add Kako login test3

--- src/routes/MainScreen.js
+++ src/routes/MainScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalFonts from "../fonts/fonts";
+import GetUser from "./GetUser";
 
 interface ContainerProps {
   width: number;
@@ -85,10 +86,12 @@ class MainScreen extends React.Component {
     this.setState({
       loginResult: true,
     });
+    console.log(this.state.loginResult);
   };
 
   loginWithKakao = () => {
     const scope = "profile_nickname,profile_image, account_email";
+    const Home = this;
     Kakao.Auth.login({
       scope,
       // success는 인증 정보를 응답(response)으로 받는다.
@@ -96,43 +99,8 @@ class MainScreen extends React.Component {
         //카카오 SDK에 사용자 토큰을 설정한다.
         window.Kakao.Auth.setAccessToken(response.access_token);
         console.log(`is set?: ${window.Kakao.Auth.getAccessToken()}`);
-
-        var ACCESS_TOKEN = window.Kakao.Auth.getAccessToken();
-
-        window.Kakao.API.request({
-          url: "/v2/user/me",
-          success: function ({ kakao_account }) {
-            //어떤 정보 넘어오는지 확인
-            console.log(kakao_account);
-            const { email, profile } = kakao_account;
-
-            console.log(email);
-            console.log(`responsed img: ${profile.profile_image_url}`);
-            console.log(profile.nickname);
-            /*
-            axios({
-              method: "post",
-              url: "/auth/sns",
-              data: {
-                id: email,
-                nickname: profile.nickname,
-                image: profile.profile_image_url,
-              },
-            })
-              .then((res) => {
-                console.log(res);
-                // history.push("/main/feed");
-              })
-              .catch((error) => {
-                // console.log(error);
-                console.error(error);
-                alert("카카오 로그인 에러?");
-              });
-              */
-          },
-          fail: function (error) {
-            console.log(error);
-          },
+        Home.setState({
+          loginResult: true,
         });
       },
       fail: function (error) {
@@ -140,11 +108,11 @@ class MainScreen extends React.Component {
         console.log(error);
       },
     });
-
     /*
     console.log("hello");
     Kakao.Auth.authorize({
       redirectUri: "http://localhost:3000/kakaoLogin",
+      
     });
     */
   };
@@ -159,14 +127,14 @@ class MainScreen extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, loginResult } = this.state;
     console.log(window.innerHeight);
     console.log(this.state.height);
     return (
       <div style={{ width: "100%", height: "100%" }}>
         <GlobalFonts />
-        {isLoading ? (
-          <div>Loading...</div>
+        {loginResult ? (
+          <GetUser></GetUser>
         ) : (
           <Container width={this.state.width} height={this.state.height}>
             <Header width={this.state.width}>
