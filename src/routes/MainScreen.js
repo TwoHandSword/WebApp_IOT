9c47e0d62cfec849b4bed13da914eea3adc7fc43
downy <TeamBehindDowny@gmail.com>
2022-01-06 00:39:37 +0900

    add Kako login test2

--- src/routes/MainScreen.js
+++ src/routes/MainScreen.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalFonts from "../fonts/fonts";
-import KakaoLogin from "../components/kakaoLogin";
 
 interface ContainerProps {
   width: number;
@@ -15,6 +14,8 @@ interface HeaderProps {
 interface MainImgProps {
   width: number;
 }
+
+const { Kakao } = window;
 
 const Container = styled.div`
   flex: 1;
@@ -80,6 +81,74 @@ class MainScreen extends React.Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   };
 
+  doLogin = () => {
+    this.setState({
+      loginResult: true,
+    });
+  };
+
+  loginWithKakao = () => {
+    const scope = "profile_nickname,profile_image, account_email";
+    Kakao.Auth.login({
+      scope,
+      // success는 인증 정보를 응답(response)으로 받는다.
+      success: function (response) {
+        //카카오 SDK에 사용자 토큰을 설정한다.
+        window.Kakao.Auth.setAccessToken(response.access_token);
+        console.log(`is set?: ${window.Kakao.Auth.getAccessToken()}`);
+
+        var ACCESS_TOKEN = window.Kakao.Auth.getAccessToken();
+
+        window.Kakao.API.request({
+          url: "/v2/user/me",
+          success: function ({ kakao_account }) {
+            //어떤 정보 넘어오는지 확인
+            console.log(kakao_account);
+            const { email, profile } = kakao_account;
+
+            console.log(email);
+            console.log(`responsed img: ${profile.profile_image_url}`);
+            console.log(profile.nickname);
+            /*
+            axios({
+              method: "post",
+              url: "/auth/sns",
+              data: {
+                id: email,
+                nickname: profile.nickname,
+                image: profile.profile_image_url,
+              },
+            })
+              .then((res) => {
+                console.log(res);
+                // history.push("/main/feed");
+              })
+              .catch((error) => {
+                // console.log(error);
+                console.error(error);
+                alert("카카오 로그인 에러?");
+              });
+              */
+          },
+          fail: function (error) {
+            console.log(error);
+          },
+        });
+      },
+      fail: function (error) {
+        console.log("에러발생");
+        console.log(error);
+      },
+    });
+
+    /*
+    console.log("hello");
+    Kakao.Auth.authorize({
+      redirectUri: "http://localhost:3000/kakaoLogin",
+    });
+    */
+  };
+
   componentDidMount() {
     this.getDatas();
     window.addEventListener("resize", this.updateDimensions);
@@ -116,7 +185,14 @@ class MainScreen extends React.Component {
               />
             </MainImg>
             <ButtonBox>
-              <KakaoLogin></KakaoLogin>
+              <div>
+                <a id="custom-login-btn" onClick={this.loginWithKakao}>
+                  <img
+                    src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+                    width="222"
+                  />
+                </a>
+              </div>
             </ButtonBox>
           </Container>
         )}
