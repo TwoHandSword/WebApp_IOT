80942c3565bdcfb9f0ddb0ff7ec602d6dc46cd7f
downy <TeamBehindDowny@gmail.com>
2022-01-07 02:53:15 +0900

    mqtt complete

--- src/routes/GetUser.js
+++ src/routes/GetUser.js
@@ -1,6 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import Amplify from "aws-amplify";
+import { AWSIoTProvider } from "@aws-amplify/pubsub/lib/Providers";
 
+function init() {
+  Amplify.configure({
+    Auth: {
+      identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+      region: process.env.REACT_APP_REGION,
+      userPoolId: process.env.REACT_APP_USER_POOL_ID,
+      userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    },
+  });
+
+  Amplify.addPluggable(
+    new AWSIoTProvider({
+      aws_pubsub_region: process.env.REACT_APP_REGION,
+      aws_pubsub_endpoint: `wss://${process.env.REACT_APP_MQTT_ID}.iot.${process.env.REACT_APP_REGION}.amazonaws.com/mqtt`,
+    })
+  );
+}
+function sub() {
+  Amplify.PubSub.subscribe("pi/1").subscribe({
+    next: (data) => console.log("Message received", data),
+    error: (error) => console.error(error),
+    close: () => console.log("Done"),
+  });
+}
 interface ContainerProps {
   width: number;
   height: number;
@@ -90,6 +116,9 @@ class GetUser extends React.Component {
         console.log(error);
       },
     });
+
+    init();
+    sub();
   }
 
   render() {
